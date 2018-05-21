0e7ed4823c6b82549df9be975a554c0472951bc4
Damian Juzyk <damian.juzyk@gmail.com>
2018-05-21 13:26:40 +0200

    add state

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -14,13 +14,13 @@ export class LogFormComponent implements OnInit {
   text: string;
   date: any;
 
+  
   isNew: boolean = true;
 
   constructor(private logService: LogService) { }
 
   ngOnInit() {
-    // Subscribe to the selectedLog observable
-    this.logService.selectedLog.subscribe(log => {
+    this.logService.selectedLog.subscribe(log =>{
       if(log.id !== null) {
         this.isNew = false;
         this.id = log.id;
@@ -29,7 +29,7 @@ export class LogFormComponent implements OnInit {
       }
     });
   }
-
+  
   onSubmit() {
     // Check if new log
     if(this.isNew) {
@@ -51,6 +51,17 @@ export class LogFormComponent implements OnInit {
       // Update log
       this.logService.updateLog(updLog);
     }
+      // Clear state
+      
+    this.clearState();
+  }
+
+  clearState() {
+    this.isNew = true;
+    this.id = '';
+    this.text = ''
+    this.date = '';
+    this.logService.clearState();
   }
 
   generateId() {
